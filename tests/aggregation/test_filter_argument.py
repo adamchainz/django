944658182f111bde045f7cef8e138fca3f38f79b
Adam Johnson <me@adamj.eu>
2025-05-21 13:47:54 +0100

    Fixed #36404 -- Fixed Aggregate.filter using OuterRef.

--- tests/aggregation/test_filter_argument.py
+++ tests/aggregation/test_filter_argument.py
@@ -182,6 +182,23 @@ class FilteredAggregateTests(TestCase):
         )
         self.assertEqual(aggregate, {"max_rating": 4.5})
 
+    def test_filtered_aggregrate_ref_in_subquery_annotation(self):
+        aggs = (
+            Author.objects.annotate(
+                count=Subquery(
+                    Book.objects.annotate(
+                        weird_count=Count(
+                            "pk",
+                            filter=Q(pages=OuterRef("age")),
+                        )
+                    ).values("weird_count")[:1]
+                ),
+            )
+            .order_by("pk")
+            .aggregate(sum=Sum("count"))
+        )
+        self.assertEqual(aggs["sum"], 0)
+
     def test_filtered_aggregate_on_exists(self):
         aggregate = Book.objects.values("publisher").aggregate(
             max_rating=Max(
